8f27cf26de94a40df152ed54e0ae5ba4c721c02c
SAR\Amit.Sharma <amit.sharma.eng14805@gmail.com>
2018-12-10 13:10:09 +0530

    Assertion File Added

---
+++ AmitsFramework/src/test/java/org/com/TestAssert.java
@@ -0,0 +1,24 @@
+package org.com;
+
+import org.openqa.selenium.By;
+import org.testng.Assert;
+
+public class TestAssert extends origin {
+
+	public void testAssertmyBlankLoginPage(){
+		String expect = pr.getProperty("logininstruction");
+		Assert.assertEquals(driver.findElement(By.xpath("//*[@id='loginForm']/div[1]/div")).getText(), expect);
+	}
+	public void testAssertmyvalidloginPage(){
+		String expect = pr.getProperty("loginexpected");
+		Assert.assertEquals(driver.getTitle(), expect);	
+	}
+	public void testAssertmyloginPageInvalidUName(){
+		String expect = pr.getProperty("nonexistinguser");
+		Assert.assertEquals(driver.findElement(By.xpath("//*[@id='messagingDiv']/div")).getText(), expect);	
+	}
+	public void testAssertmyloginPageInvalidPassworde(){
+		String expect = pr.getProperty("nonexistinguser");
+		Assert.assertEquals(driver.findElement(By.xpath("//*[@id='messagingDiv']/div")).getText(), expect);	
+	}
+}
